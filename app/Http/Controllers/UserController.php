65841739011db3226a0f475493a4fd300504121a
elfeny7 <ikmalfaris50@gmail.com>
2025-09-14 23:35:51 +0700

    fix(auth, user): authorization exception

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Interfaces\UserServiceInterface;
 use App\Http\Requests\Auth\StoreUserRequest;
 use App\Http\Requests\Auth\UpdateUserRequest;
 use App\Http\Resources\UserResource;
+use Illuminate\Auth\Access\AuthorizationException;
 
 class UserController extends Controller
 {
@@ -23,6 +24,8 @@ class UserController extends Controller
             $this->authorize('manage', 'manage-user');
             $users = $this->userServiceInterface->getAllUsers();
             return ApiResponse::success(UserResource::collection($users), 'Users Retrieved', 200);
+        } catch (AuthorizationException $e) {
+            return ApiResponse::throw($e, 'Unauthorized', 403);
         } catch (\Exception $e) {
             return ApiResponse::throw($e);
         }
@@ -34,6 +37,8 @@ class UserController extends Controller
             $this->authorize('manage', 'manage-user');
             $user = $this->userServiceInterface->getUserById($id);
             return ApiResponse::success(new UserResource($user), 'User Retrieved', 200);
+        } catch (AuthorizationException $e) {
+            return ApiResponse::throw($e, 'Unauthorized', 403);
         } catch (\Exception $e) {
             return ApiResponse::throw($e);
         }
@@ -45,6 +50,8 @@ class UserController extends Controller
             $this->authorize('manage', 'manage-user');
             $user = $this->userServiceInterface->createUser($request->getStorePayload());
             return ApiResponse::success(new UserResource($user), 'User Create successsful', 201);
+        } catch (AuthorizationException $e) {
+            return ApiResponse::throw($e, 'Unauthorized', 403);
         } catch (\Exception $e) {
             return ApiResponse::throw($e);
         }
@@ -56,6 +63,8 @@ class UserController extends Controller
             $this->authorize('manage', 'manage-user');
             $this->userServiceInterface->updateUser($request->getUpdatePayload(), $id);
             return ApiResponse::success('', 'User Update successsful', 200);
+        } catch (AuthorizationException $e) {
+            return ApiResponse::throw($e, 'Unauthorized', 403);
         } catch (\Exception $e) {
             return ApiResponse::throw($e);
         }
@@ -67,6 +76,8 @@ class UserController extends Controller
             $this->authorize('manage', 'manage-user');
             $this->userServiceInterface->deleteUser($id);
             return ApiResponse::success('', 'User Delete successsful', 204);
+        } catch (AuthorizationException $e) {
+            return ApiResponse::throw($e, 'Unauthorized', 403);
         } catch (\Exception $e) {
             return ApiResponse::throw($e);
         }
