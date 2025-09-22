688635170fb6c093256772eb4bc0a7de93cbaf4c
elfeny7 <ikmalfaris50@gmail.com>
2025-09-22 17:34:38 +0700

    fix(auth, middleware): remove only catch exception api responsen

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -5,11 +5,19 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Interfaces\TokenServiceInterface;
 
 class JwtMiddleware
 {
+    private TokenServiceInterface $tokenServiceInterface;
+
+    public function __construct(TokenServiceInterface $tokenServiceInterface){
+        $this->tokenServiceInterface = $tokenServiceInterface;
+    }
+
     public function handle(Request $request, Closure $next): Response
     {
+        $this->tokenServiceInterface->authenticate();
         return $next($request);
     }
 }
