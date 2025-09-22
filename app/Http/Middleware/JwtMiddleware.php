5b2964ed0fa67737cfc56156066d33d260d861f6
elfeny7 <ikmalfaris50@gmail.com>
2025-09-22 16:37:31 +0700

    refactor(auth, middleware): remove response handler on jwt middleware

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -5,24 +5,11 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Support\ApiResponse;
-use App\Interfaces\TokenServiceInterface;
 
 class JwtMiddleware
 {
-    private TokenServiceInterface $tokenServiceInterface;
-
-    public function __construct(TokenServiceInterface $tokenServiceInterface){
-        $this->tokenServiceInterface = $tokenServiceInterface;
-    }
-
     public function handle(Request $request, Closure $next): Response
     {
-        try {
-            $this->tokenServiceInterface->authenticate();
-        } catch (\Exception $e) {
-            return ApiResponse::throw($e, 'Unauthenticated', 401);
-        }
         return $next($request);
     }
 }
